b99bb6d3c19b27b3cecab49187f01e4f15114c96
iamChintan <88285492+iamChintan@users.noreply.github.com>
2023-02-21 19:09:45 -0500

    pojo class
    pojo/model class generated

--- app/src/main/java/com/example/restapi/Activity/MainActivity.java
+++ app/src/main/java/com/example/restapi/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.restapi;
+package com.example.restapi.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.restapi.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/restapi/Utils/Common.java
@@ -0,0 +1,5 @@
+package com.example.restapi.Utils;
+
+public class Common {
+    public  static String API_KEY = "68d7b54ac1e043b6cf2378bba0d6c5e8";
+}
